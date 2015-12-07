e3e364bb7513a19f6bf7860f185c0201078f8b63
jingyu11 <jingyu11@staff.weibo.com>
2015-12-07 17:03:24 +0800

    地图test

--- WeShow/Classes/MainViewControllers/MainViewController.m
+++ WeShow/Classes/MainViewControllers/MainViewController.m
@@ -12,6 +12,7 @@
 @interface MainViewController ()<MAMapViewDelegate>
 {
     MAMapView *_mapView;
+    UIButton * arrowButton;
 }
 @end
 
@@ -29,13 +30,27 @@
 {
     [super viewDidAppear:animated];
     // Do any additional setup after loading the view, typically from a nib.
-    //配置用户Key
     [MAMapServices sharedServices].apiKey = mapAPIKEY;
     
     _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
     _mapView.delegate = self;
+    _mapView.showsUserLocation = YES;    //YES 为打开定位，NO为关闭定位
+    [_mapView setUserTrackingMode: MAUserTrackingModeFollow animated:YES]; //地图跟着位置移动
+    [_mapView setZoomLevel:16.1 animated:YES];
+    
+    //构造圆
+    MACircle *circle = [MACircle circleWithCenterCoordinate:CLLocationCoordinate2DMake(39.952136, 116.50095) radius:500];
+    
+    //在地图上添加圆
+    [_mapView addOverlay: circle];
     
     [self.view addSubview:_mapView];
+}
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    
 }
 
 - (void)refesh{
@@ -44,6 +59,52 @@
     }];
 }
 
+//圆形覆盖物
+- (MAOverlayView *)mapView:(MAMapView *)mapView viewForOverlay:(id <MAOverlay>)overlay
+{
+    if ([overlay isKindOfClass:[MACircle class]])
+    {
+        MACircleView *circleView = [[MACircleView alloc] initWithCircle:overlay];
+        
+        circleView.lineWidth = 5.f;
+        circleView.strokeColor = [UIColor colorWithRed:0.0 green:0.0 blue:1.0 alpha:0.5];
+        circleView.fillColor = [UIColor colorWithRed:0.0 green:0.0 blue:1.0 alpha:0.5];
+        //circleView.lineDash = YES;
+        
+        return circleView;
+    }
+    return nil;
+}
 
+//位置更新回调
+-(void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation
+{
+    if(updatingLocation)
+    {
+        //取出当前位置的坐标
+        NSLog(@"latitude : %f,longitude: %f",userLocation.coordinate.latitude,userLocation.coordinate.longitude);
+    }
+}
+
+//用户定位
+- (void)mapView:(MAMapView *)mapView didAddAnnotationViews:(NSArray *)views
+{
+    MAAnnotationView *view = views[0];
+    
+    // 放到该方法中用以保证userlocation的annotationView已经添加到地图上了。
+    if ([view.annotation isKindOfClass:[MAUserLocation class]])
+    {
+        MAUserLocationRepresentation *pre = [[MAUserLocationRepresentation alloc] init];
+        pre.fillColor = [UIColor colorWithRed:0.9 green:0.1 blue:0.1 alpha:0.3];
+        pre.strokeColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.9 alpha:1.0];
+        pre.image = [UIImage imageNamed:@"location.png"];
+        pre.lineWidth = 3;
+        pre.lineDashPattern = @[@6, @3];
+        
+        [_mapView updateUserLocationRepresentation:pre];
+        
+        view.calloutOffset = CGPointMake(0, 0);
+    } 
+}
 
 @end
