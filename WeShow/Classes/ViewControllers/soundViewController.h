aeadd16a075c50936f885b1862fc11555b7de739
[jingyu11] <[jingyu11@staff.weibo.com]>
2015-12-09 18:29:32 +0800

    ani修改，line有问题

---
+++ WeShow/Classes/ViewControllers/soundViewController.h
@@ -0,0 +1,15 @@
+//
+//  soundViewController.h
+//  WeShow
+//
+//  Created by insomnia on 15/12/9.
+//  Copyright © 2015年 Weibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface soundViewController : UIViewController
+
+- (instancetype) initWithMediaUrl:(NSURL*)url;
+
+@end
---
+++ WeShow/Classes/ViewControllers/soundViewController.m
@@ -0,0 +1,116 @@
+//
+//  soundViewController.m
+//  WeShow
+//
+//  Created by insomnia on 15/12/9.
+//  Copyright © 2015年 Weibo. All rights reserved.
+//
+
+#import "soundViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface soundViewController ()
+@property (strong, nonatomic) NSURL* mediaUrl;
+
+@property (strong, nonatomic) AVPlayer *avPlayer;
+@property (strong, nonatomic) AVPlayerLayer *avPlayerLayer;
+@property (strong, nonatomic) UIButton *cancelButton;
+
+@end
+
+@implementation soundViewController
+
+- (instancetype) initWithMediaUrl:(NSURL*)url
+{
+    if (self = [super init])
+    {
+        _mediaUrl = url;
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // the video player
+    self.avPlayer = [AVPlayer playerWithURL:self.mediaUrl];
+    self.avPlayer.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    
+    self.avPlayerLayer = [AVPlayerLayer playerLayerWithPlayer:self.avPlayer];
+    //self.avPlayerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[self.avPlayer currentItem]];
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    
+    self.avPlayerLayer.frame = CGRectMake(0, 0, screenRect.size.width, screenRect.size.height);
+    [self.view.layer addSublayer:self.avPlayerLayer];
+    
+    // cancel button
+    [self.view addSubview:self.cancelButton];
+    [self.cancelButton addTarget:self action:@selector(cancelButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    self.cancelButton.frame = CGRectMake(0, 0, 44, 44);
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self.avPlayer play];
+}
+
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (UIButton *)cancelButton {
+    if(!_cancelButton) {
+        UIImage *cancelImage = [UIImage imageNamed:@"map_create.png"];
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        button.tintColor = [UIColor whiteColor];
+        [button setImage:cancelImage forState:UIControlStateNormal];
+        button.imageView.clipsToBounds = NO;
+        button.contentEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10);
+        button.layer.shadowColor = [UIColor blackColor].CGColor;
+        button.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
+        button.layer.shadowOpacity = 0.4f;
+        button.layer.shadowRadius = 1.0f;
+        button.clipsToBounds = NO;
+        
+        _cancelButton = button;
+    }
+    
+    return _cancelButton;
+}
+
+- (void)cancelButtonPressed:(UIButton *)button {
+    NSLog(@"cancel button pressed!");
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
