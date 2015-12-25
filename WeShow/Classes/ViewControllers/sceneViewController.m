1a2e28e3795d012f9979dafff182c9a6c45f6ed5
[jingyu11] <[jingyu11@staff.weibo.com]>
2015-12-26 03:00:51 +0800

    去掉播放

--- WeShow/Classes/ViewControllers/sceneViewController.m
+++ WeShow/Classes/ViewControllers/sceneViewController.m
@@ -357,14 +357,12 @@
 
 - (void)createVideo
 {
-    cameraViewController *VC = [[cameraViewController alloc]init];
-    [self presentViewController:VC animated:NO completion:^{}];
-//    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-//    _toastView = [[UIImageView alloc]initWithFrame:CGRectZero];
-//    _toastView.image = [UIImage imageNamed:@"video_push_red.png"];
-//    [window addSubview:_toastView];
-//    [self showAnimation];
-//    [self performSelector:@selector(hideAnimation:) withObject:0 afterDelay:1];
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    _toastView = [[UIImageView alloc]initWithFrame:CGRectZero];
+    _toastView.image = [UIImage imageNamed:@"video_push_red.png"];
+    [window addSubview:_toastView];
+    [self showAnimation];
+    [self performSelector:@selector(hideAnimation:) withObject:0 afterDelay:1];
 }
 
 -(void)showAnimation{
